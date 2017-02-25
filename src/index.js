e5984837e19666a01ffe2a93eab083e2e5846845
gsingh737 <ssgurpreetsingh@gmail.com>
2017-02-25 13:28:58 -0500

    First React Youtube Search app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+/**
+ * Created by User on 2/23/2017.
+ */
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './component/search_bar';
+import VideoList from './component/video_list';
+import VideoDetail from './component/video_detail';
+const API_KEY = '';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component. This component should produce html
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards');
+    }
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term},  (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+    render(){
+        const videoSearch = _.debounce((term)   => { this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+//Take this component html and put it on the page (in DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
